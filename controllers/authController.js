ce92850ca40f2e5852f9e12c943b64cf3214a7d9
Sahil Kalkal <sahilkalkal108@gmail.com>
2026-05-02 14:28:01 +0530

    latest code

--- controllers/authController.js
+++ controllers/authController.js
@@ -160,7 +160,7 @@ export const login = async (req, res) => {
       });
     }
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    const isPasswordMatch = await bcrypt.compare(password, user.pin);
 
     if (!isPasswordMatch) {
       return res.status(401).json({
