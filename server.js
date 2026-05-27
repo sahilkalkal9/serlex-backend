d9ef05d841b068abc18a3146a624a79bd04c3d6e
Apple <apple@MacBook-Pro.local>
2026-05-27 12:57:04 +0530

    latest code

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Server is running new and healthy",
+    message: "Server is running healthy!!",
   });
 });
 
